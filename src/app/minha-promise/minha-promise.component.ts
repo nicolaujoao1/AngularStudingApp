9fd596f811d19f7896228004e4b6e1d8e00b46a1
nicolaujoao1 <ilolanicolau1999@gmail.com>
2023-03-06 12:08:26 +0100

    Trabalhando com usubscribe()

--- src/app/minha-promise/minha-promise.component.ts
+++ src/app/minha-promise/minha-promise.component.ts
@@ -20,12 +20,29 @@ myPromise(name:string):Promise<string>{
   });
 }
 
+usuarioObservable(nome:string, email:string):Observable<Usuario>{
+  return new Observable(
+    subscribe=>{
+      let usuario=new Usuario(nome, email);
+      if(nome==='Admin'){
+        setTimeout(()=>subscribe.next(usuario),1000);
+        setTimeout(()=>subscribe.next(usuario),2000);
+        setTimeout(()=>subscribe.next(usuario),3000);
+        setTimeout(()=>subscribe.next(usuario),4000);
+        setTimeout(()=>subscribe.complete(),5000);
+      }
+
+    }
+  );
+}
+
  myObservable(name:string):Observable<string>{
   return new Observable(subscribe=>{
       if(name==='Ilola'){
         subscribe.next(`Olá ${name}`);
         subscribe.next(`Ola novamente ${name}`);
-        setTimeout(()=> subscribe.next(`Olá com Delay ${name}`),5000);
+        setTimeout(()=> subscribe.next(`Olá com Delay ${name}`),10000);
+        subscribe.complete();
       }
       else{
         subscribe.error('Ops! Oucorreu um erro');
@@ -34,16 +51,36 @@ myPromise(name:string):Promise<string>{
  }
 
  ngOnInit(): void {
-    //  this.myObservable('')
+    
+   const obs= this.usuarioObservable('Admin','admin@gmail.com');
+   const subs= obs.subscribe(
+     result=>console.log(result),
+     error=>console.log(error),
+     ()=>console.log("FIM")
+   );
+   setTimeout(() => {
+     subs.unsubscribe();
+   }, 3500);
+  //  this.myObservable('Ilola')
     //  .subscribe(
     //   result=>console.log(result),
-    //   error=>console.log(error)
+    //   error=>console.log(error),
+    //  ()=>console.log('FIM')
     //   );
  
-    this.myPromise('Ilola')
-    .then(result=>console.log(result))
-    .catch(error=>console.log(error));
+    // this.myPromise('Ilola')
+    // .then(result=>console.log(result))
+    // .catch(error=>console.log(error));
      
- }
-
+     }  
+ 
+}
+export class Usuario{
+  private nome:string;
+  private email:string;  
+  constructor(nome:string, email:string){
+    this.nome=nome;
+    this.email=email;
+  }
+  
 }
